f70677967a661c7aa3a05b8f5764e09871e76ad4
danielplebani <danielpleba03@gmail.com>
2023-10-27 17:54:41 +0200

    add debug

--- index.php
+++ index.php
@@ -10,7 +10,24 @@ Bonus (non opzionale):
 organizzate il progetto come visto stamattina a lezione usando varie sottocartelle per inserire classi, layout e dati 
 -->
 
+
+
+
+
+
+
 <?php
+
+
+$debug = true; // cambiate in true e false in base se volete o meno vedere gli errori
+if ($debug == true) {
+    ini_set('display_errors', 1); // imposta il file php.ini a mostrare gli errori
+    error_reporting(E_ALL); // imposta il livello di errori da mostrare E_ALL tutti
+}
+
+
+
+
 require __DIR__ . './models/shop.php';
 require __DIR__ . './db.php';
 ?>
